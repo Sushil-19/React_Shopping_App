79f8f43880ea443927763e8f28a1f65b5caed01e
Sushil-19 <dubesushil19007@gmail.com>
2024-04-17 13:56:52 +0530

    The Frontend is completed now

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './Pages/Home';
-import Success from './Pages/Success';
-import NotFound from './Pages/NotFound';
-
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './Pages/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Success from './Pages/Success/Success';
+import './App.css'
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/success' element={<Success/>}></Route>
-        <Route path='*' element={<NotFound/>}></Route>
-      </Routes>
-      <Toaster />
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/success' element={<Success/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+        <Toaster/>
+      </Router>
+    </>
   )
 }
 
